1874663f52246937e6a125797b8fa1a4fe4a0f63
lekyvong09 <lekyvong09@gmail.com>
2022-12-13 21:47:28 +0700

    AspectJ AOP 1

---
+++ src/main/java/com/ray/crm/aspect/LoggingAspect.java
@@ -0,0 +1,33 @@
+package com.ray.crm.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+	private static final Logger myLogger = LogManager.getLogger(LoggingAspect.class);
+	
+	@Pointcut("execution(* com.ray.crm.controller.*.*(..))")
+	private void forControllerPackage() { }
+	
+	@Pointcut("execution(* com.ray.crm.dao.*.*(..))")
+	private void forDaoPackage() { }
+	
+	@Pointcut("execution(* com.ray.crm.service.*.*(..))")
+	private void forServicePackage() { }
+	
+	@Pointcut("forControllerPackage() || forDaoPackage() || forServicePackage()")
+	private void forLoggingAspect() { }
+	
+	@Before("forLoggingAspect()")
+	public void beforeLogging(JoinPoint theJoinPoint) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("==> before advice: calling method: " + theMethod);
+	}
+}
